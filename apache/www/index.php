e58eae46580e004bd36e65631dc3c4d96c2e48e9
Hutger H <hutger@gmail.com>
2017-11-06 13:51:07 -0300

    Mudando senha do banco

--- apache/www/index.php
+++ apache/www/index.php
@@ -1,5 +1,5 @@
 <?php
   echo "<pre><h2>Criando um ambiente de desenvolvimento PHP com Docker Compose.</h2></pre><br/>";
-  mysqli_connect("mysql", "root", "kermit") or die(mysqli_error()); 
+  mysqli_connect("mysql", "root", "12345") or die(mysqli_error()); 
   echo "<pre><h3>Connected to MySQL<h3></pre><br/>";
 ?>
