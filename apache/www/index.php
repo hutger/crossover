c523c17455d667013ef562c67b6192b37831f8a3
Hutger H <hutger@gmail.com>
2017-11-06 00:06:56 -0300

    Novas atualizações

--- apache/www/index.php
+++ apache/www/index.php
@@ -1,5 +1,5 @@
 <?php
   echo "<pre><h2>Criando um ambiente de desenvolvimento PHP com Docker Compose.</h2></pre><br/>";
-  mysqli_connect("db", "root", "kermit") or die(mysqli_error()); 
+  mysqli_connect("mysql", "root", "kermit") or die(mysqli_error()); 
   echo "<pre><h3>Connected to MySQL<h3></pre><br/>";
 ?>
