ed200380a469291ad40a91149fe6accde9b0a8c4
Hutger H <hutger@gmail.com>
2017-11-06 14:49:28 -0300

    Mais ajustes.

--- apache/www/index.php
+++ apache/www/index.php
@@ -1,5 +1,5 @@
 <?php
   echo "<pre><h2>Apache+PHP Container Running</h2></pre><br/>";
-  mysqli_connect("mysql", "root", "12345") or die(mysqli_error()); 
+  mysqli_connect("mysql", "root", "cross12345") or die(mysqli_error()); 
   echo "<pre><h3>Connected to MySQL<h3></pre><br/>";
 ?>
